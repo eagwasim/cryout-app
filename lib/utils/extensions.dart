70c267b119b61cc2aead02ca2ed83322d019d612
Agwasim Emmanuel <agwasim.e@gmail.com>
2020-09-14 12:42:55 +0200

    bug fix

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -3,6 +3,9 @@ extension StringExtension on String {
     if (this.length <= 1) return this.toUpperCase();
     var words = this.toLowerCase().split(' ');
     var capitalized = words.map((word) {
+      if (word.length < 2) {
+        return word;
+      }
       var first = word.substring(0, 1).toUpperCase();
       var rest = word.substring(1);
       return '$first$rest';
